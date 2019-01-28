108ed8e78cf6e04d9e9c624d107a7427128787c7
Robin Savage <macbookbro@Robins-MBP.domain>
2019-01-28 14:27:30 -0600

    Answer

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,11 +59,20 @@ Instructions:
 
   window.addEventListener('WebComponentsReady', function() {
     home = document.querySelector('section[data-route="home"]');
-    /*
-    Uncomment the next line and start here when you're ready to add the first thumbnail!
-
-    Your code goes here!
-     */
-    // getJSON('../data/earth-like-results.json')
+    
+    getJSON('https://github.com/savagewebdev/exoplanet-explorer/blob/xhr-start/app/data/earth-like-results.json');
+    .then(function(response) { // Following the JSON response promise ...
+      addSearchHeader(response.query); // Familiar portion. Adding of the search header. 
+      return getJSON(response.results[0]); // Returns the second getJSON. Gets the url of the first planet.
+      )}
+    .catch(function() { // First catch is in case there is an error with the search results.
+      throw error('Search Request Error');
+      })
+    .then(createPlanetThumb); // By returning the second getJSON, it gets passed to this .then().
+    
+    .catch(function(e) { // Second catch is in case there is anything else that goes wrong.
+      addSearchHeader('Unknown');
+      console.log(e);
+    });
   });
 })(document);
